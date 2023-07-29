f8a6ba83f55887b1c391cfc746bb746dab088b21
Mathieu Lemay <acidrain1@gmail.com>
2023-07-29 10:59:03 -0400

    Update pydantic to 2.x

--- catchall_api/__init__.py
+++ catchall_api/__init__.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 __project_name__ = "catchall_api"
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_prefix="CATCHALL_API_", populate_by_name=True
+    )
+
     log_level: str = "INFO"
-    log_no_color: str | None = Field(default=None, env="NO_COLOR")
+    log_no_color: str | None = Field(default=None, validation_alias="NO_COLOR")
     log_to_file: bool = False
     log_file_directory: Path = Path("/output")
-
-    class Config:
-        env_file = ".env"
-        env_prefix = "CATCHALL_API_"
